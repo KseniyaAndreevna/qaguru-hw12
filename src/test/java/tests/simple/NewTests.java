5969368a10291575872ee113fc15e6a5aeec4720
ksu <bindyurova.kseniya@gmail.com>
2022-02-06 17:38:07 +0700

    add new simple test

---
+++ src/test/java/tests/simple/NewTests.java
@@ -0,0 +1,12 @@
+package tests.simple;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class NewTests {
+
+    @Test
+    void simpleTest() {
+        Assertions.assertTrue(1 == 1);
+    }
+}
